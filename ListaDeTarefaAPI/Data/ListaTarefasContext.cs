b85d7b70200dd0e454da6b561f0743be59f085f9
DevErick <djerickdev@gmail.com>
2025-02-05 10:15:51 -0300

    Alteração de Tarefas concluidas por um endpoint que retrone pelo Id

---
+++ ListaDeTarefaAPI/Data/ListaTarefasContext.cs
@@ -0,0 +1,16 @@
+﻿using ListaDeTarefaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTarefaAPI.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext(
+            DbContextOptions<ListaTarefasContext> options) : base
+            (options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TipoTarefa> TipoTarefas { get; set; }
+    }
+}
